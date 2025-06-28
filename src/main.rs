a8fd5c9269b45c8adcb3c9b34e92cda56414b7aa
rodrigocitadin <citadinrodrigo@gmail.com>
2025-06-28 02:57:37 -0300

    feat: basic structs and enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#[derive(Debug)]
+enum TcpState {
+    Closed,
+    SynSent,
+    SynReceived,
+    Established,
+}
+
+#[derive(Debug, Clone)]
+struct TcpSegment {
+    src_port: u16,
+    dst_port: u16,
+    seq: u32,
+    ack: u32,
+    syn: bool,
+    ack_flag: bool,
+    payload: Vec<u8>,
+}
+
+struct PseudoTcpClient {
+    port: u16,
+    state: TcpState,
+    seq: u32,
+    ack: u32,
+    peer_seq: u32,
+}
+
 fn main() {
     println!("Hello, world!");
 }
+
