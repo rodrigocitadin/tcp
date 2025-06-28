5ebe5387ab8a5544b7029abdc62b4b7de7c397b5
rodrigocitadin <citadinrodrigo@gmail.com>
2025-06-28 02:58:24 -0300

    feat: tcp segment with syn, syn + ack, ack and data

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,56 @@ struct TcpSegment {
     payload: Vec<u8>,
 }
 
+impl TcpSegment {
+    fn syn(src: u16, dst: u16, seq: u32) -> Self {
+        Self {
+            src_port: src,
+            dst_port: dst,
+            seq,
+            ack: 0,
+            syn: true,
+            ack_flag: false,
+            payload: vec![],
+        }
+    }
+
+    fn syn_ack(src: u16, dst: u16, seq: u32, ack: u32) -> Self {
+        Self {
+            src_port: src,
+            dst_port: dst,
+            seq,
+            ack,
+            syn: true,
+            ack_flag: true,
+            payload: vec![],
+        }
+    }
+
+    fn ack(src: u16, dst: u16, seq: u32, ack: u32) -> Self {
+        Self {
+            src_port: src,
+            dst_port: dst,
+            seq,
+            ack,
+            syn: false,
+            ack_flag: true,
+            payload: vec![],
+        }
+    }
+
+    fn data(src: u16, dst: u16, seq: u32, ack: u32, data: Vec<u8>) -> Self {
+        Self {
+            src_port: src,
+            dst_port: dst,
+            seq,
+            ack,
+            syn: false,
+            ack_flag: true,
+            payload: data,
+        }
+    }
+}
+
 struct PseudoTcpClient {
     port: u16,
     state: TcpState,
